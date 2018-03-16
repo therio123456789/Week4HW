2217758d10f3599a457d5c392356a6f1c17df706
Rio <Therio123456789@gmail.com>
2018-03-16 19:11:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rio/week3hw/Film.java
@@ -0,0 +1,95 @@
+package com.example.rio.week3hw;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rio on 3/15/2018.
+ */
+
+
+public class Film{
+    public final static String HostImage = " http://image.tmdb.org/t/p/w500";
+    private String title;
+    private String overview;
+    private Boolean isVideo;
+    private String posterPath;
+    private String backdropPath;
+    private String releaseDate;
+    private Float voteAverage;
+    public Film(String title, String overview, Boolean isVideo, String posterPath, String backdropPath,
+                String releaseDate, Float voteAverage) {
+        super();
+        this.title = title;
+        this.overview = overview;
+        this.isVideo = isVideo;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+        this.releaseDate = releaseDate;
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+    public Boolean getIsVideo() {
+        return isVideo;
+    }
+    public void setIsVideo(Boolean isVideo) {
+        this.isVideo = isVideo;
+    }
+    public String getPosterPath() {
+        return posterPath;
+    }
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public Float getVoteAverage() {
+        return voteAverage;
+    }
+    public void setVoteAverage(Float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+    public static ArrayList<Film> getListFilmFromJson(String jsonString) throws JsonParseException {
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonMaterial = jsonParser.parse(jsonString).getAsJsonObject();
+        JsonArray jsonFilmArray = jsonMaterial.get("results").getAsJsonArray();
+        ArrayList<Film> films = new ArrayList<>();
+        if(!jsonFilmArray.isJsonArray())
+            return null;
+        else {
+            for(int i = 0; i < jsonFilmArray.size(); i++ ) {
+                JsonObject jsonObject = jsonFilmArray.get(i).getAsJsonObject();
+                films.add(new Film(jsonObject.get("title").getAsString(), jsonObject.get("overview").getAsString(), jsonObject.get("video").getAsBoolean()
+                        , jsonObject.get("poster_path").getAsString(), jsonObject.get("backdrop_path").getAsString(), jsonObject.get("release_date").getAsString(), jsonObject.get("vote_average").getAsFloat()));
+            }
+            return films;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/rio/week3hw/RecyclerDataAdapter.java
@@ -0,0 +1,76 @@
+package com.example.rio.week3hw;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Rio on 3/15/2018.
+ */
+
+public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.DataViewHolder> {
+
+    private Context mcontext;
+    private List<Film> films;
+
+    public RecyclerDataAdapter(Context context, ArrayList<Film> films) {
+        this.mcontext = context;
+        this.films = films;
+    }
+
+    @Override
+    public DataViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        itemView = layoutInflater.inflate(R.layout.item_film,parent,false);
+        return new DataViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(DataViewHolder holder, int position) {
+        Film film = films.get(position);
+        String title = film.getTitle();
+        String overView = film.getOverview();
+        String posterPath = film.HostImage + film.getPosterPath();
+        Boolean isPlay = film.getIsVideo();
+        holder.textTitle.setText(title);
+        holder.textOverview.setText(overView);
+        Picasso.with(mcontext).load(posterPath).into(holder.imPoster);
+        holder.imPlay.setImageResource(android.R.drawable.ic_media_play);
+        if(isPlay) {
+            holder.imPlay.setVisibility(View.VISIBLE);
+        } else {
+            holder.imPlay.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return films == null ? 0: films.size();
+    }
+;
+    public class DataViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imPoster;
+        private ImageView imPlay;
+        private TextView textTitle;
+        private TextView textOverview;
+        public DataViewHolder(View itemView) {
+            super(itemView);
+            imPoster = (ImageView) itemView.findViewById(R.id.imgPoster);
+            imPlay = (ImageView) itemView.findViewById(R.id.imgPlay);
+            textTitle = (TextView) itemView.findViewById(R.id.txtTitle);
+            textOverview = (TextView) itemView.findViewById(R.id.txtOverview);
+        }
+    }
+}
